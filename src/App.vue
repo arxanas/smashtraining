b476b80e0e6b42941fadde55b6ac0081317fdbed
Waleed Khan <me@waleedkhan.name>
2019-08-31 12:30:43 +0200

    dev: add Prettier for auto-formatting files

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,13 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
