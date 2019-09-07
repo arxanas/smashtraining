2b727788cb794d0aa19c149a2f9146d468aac832
Waleed Khan <me@waleedkhan.name>
2019-09-08 00:04:38 +0100

    feat(training): add Github issue report banner
    The idea is to crowdsource tech info that I've missed, or to fix up any broken links which might arise.

---
+++ src/components/GithubReportIssueBanner.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container fluid class="body-2 py-0">
+    <v-row align="center" justify="center">
+      <v-col :cols="12" :md="6" class="text-center text-md-left pb-1 py-md-2">
+        <slot />
+      </v-col>
+      <v-col :cols="12" :md="6" class="text-center text-md-right pt-1 py-md-2">
+        <v-btn text small color="primary" :href="issueUrl">
+          <v-icon left>mdi-github-circle</v-icon>
+          Report an issue
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data: () => ({
+    issueUrl: "https://github.com/arxanas/smashtraining/issues",
+  }),
+});
+</script>
