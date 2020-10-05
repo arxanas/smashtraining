e0990dd281704bd7656d852d289dc3668999c4ab
Waleed Khan <me@waleedkhan.name>
2020-10-05 12:41:07 -0700

    build: amend previous commit in update-gsp.sh

--- bin/update-gsp.sh
+++ bin/update-gsp.sh
@@ -5,7 +5,13 @@ git pull
 npm run build-gsp
 npx prettier --write ./src/gsp/EliteGspData.ts
 git add .
-git commit \
-  --message "build: update GSP $(date)" \
-  --author "Smash Training Bot <smashtraining@waleedkhan.name>"
-git push
+
+MESSAGE="build: update GSP $(date)"
+AUTHOR="Smash Training Bot <smashtraining@waleedkhan.name>"
+if git log -1 --pretty=%B | grep 'build: update GSP'; then
+  git commit --amend --message "$MESSAGE" --author "$AUTHOR"
+  git push -f
+else
+  git commit --message "$MESSAGE" --author "$AUTHOR"
+  git push
+fi
