ebca57be1fae97d6824e64c6c88ef8df648f26bd
Waleed Khan <wkhan@twitter.com>
2020-12-20 17:50:13 -0600

    build: update `update-gsp.sh` script for CI

--- bin/update-gsp.sh
+++ bin/update-gsp.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 set -euo pipefail
 
+export GIT_AUTHOR_NAME="Smash Training Bot"
+export GIT_AUTHOR_EMAIL="smashtraining@waleedkhan.name"
+export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
+export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
+
 git pull
 npm run build-gsp
 npx prettier --write ./src/gsp/EliteGspData.ts
 git add .
 
 MESSAGE="build: update GSP $(date)"
-AUTHOR="Smash Training Bot <smashtraining@waleedkhan.name>"
 if git log -1 --pretty=%B | grep 'build: update GSP'; then
-  git commit --amend --message "$MESSAGE" --author "$AUTHOR"
+  git commit --amend --message "$MESSAGE"
   git push -f
 else
-  git commit --message "$MESSAGE" --author "$AUTHOR"
+  git commit --message "$MESSAGE"
   git push
 fi
